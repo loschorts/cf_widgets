dc4dc1836643371d1190ee6e3479dd723be8ffdf
Ken Tran <loschorts@gmail.com>
2017-03-04 11:50:41 -0800

    start script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # user credentials are written into a .gitignored file
 # server.js writes these credentials to header when issuing requests to Cloudflare API
 
+echo "1) Store Auth Headers in ENV for server use"
 echo  -n X-Auth-Email: 
 read email
 echo
@@ -12,3 +13,8 @@ echo
 
 export xAuthEmail=$email
 export xAuthKey=$key
+
+echo "2) npm install && webpack"
+npm install && webpack
+echo "3) start node server.js "
+node server.js
